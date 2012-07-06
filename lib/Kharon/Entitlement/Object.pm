fb23a38d6ec4a470c50ce68d625e33eb6376fd1b
Roland C. Dowdeswell <elric@imrryr.org>
2012-07-06 17:01:38 +0100

    Implement Kharon::Entitlement::Object.
    Implement a ACL that takes an object and will invoke methods in it
    to return the entitlement status.  Kharon::Entitlement::Object when
    called will look for methods of the form KHARON_COMMON_ACL and
    KHARON_ACL_<verb>.  It will first invoke KHARON_COMMON_ACL giving
    it the arguments ($verb, @args).  If this returns a defined value,
    it will be used as the entitlement status.  If not, then it will
    invoke KHARON_ACL_<verb> in the same way.  If this is not found,
    then it will return 0 (Permission denied).

--- lib/Kharon/Entitlement/Object.pm
+++ lib/Kharon/Entitlement/Object.pm
@@ -1,9 +1,14 @@
 #
-# Implement a ACL superclass which can be inherited by subclasses that
-# want to use a bit of OO to define the ACLs.
-#
+# Implement a ACL that takes an object and will invoke methods in it
+# to return the entitlement status.  Kharon::Entitlement::Object when
+# called will look for methods of the form KHARON_COMMON_ACL and
+# KHARON_ACL_<verb>.  It will first invoke KHARON_COMMON_ACL giving
+# it the arguments ($verb, @args).  If this returns a defined value,
+# it will be used as the entitlement status.  If not, then it will
+# invoke KHARON_ACL_<verb> in the same way.  If this is not found,
+# then it will return 0 (Permission denied).
 
-package Kharon::Entitlement::Super;
+package Kharon::Entitlement::Object;
 use base qw(Kharon::Entitlement);
 
 use warnings;
@@ -15,22 +20,35 @@ sub new {
 
 	my $self = $class->SUPER::new(%args);
 
+	$args{subobject} = $args{subobject};
+
 	bless($self, $class);
 	return $self;
 }
 
+sub set_subobject {
+	my ($self, $subobject) = @_;
+
+	$self->{subobject} = $subobject;
+}
+
 sub check1 {
 	my ($self, $verb, @args) = @_;
+	my $subobj = $self->{subobject};
+	my $acl;
 
-	#
-	# In this module, we assume that we are simply inherited by
-	# a module that defines methods ACL_$verb which will take @args
-	# and return whether the entitlement passes.
-
-	my $acl = $self->can("ACL_$verb");
-
+	$acl = $subobject->can("KHARON_COMMON_ACL");
 	if (defined($acl)) {
-		return $acl($self, @args);
+		my $ret = $acl($self, $verb, @args);
+
+		if (defined($ret)) {
+			return $ret;
+		}
+	}
+
+	$acl = $subobject->can("KHARON_ACL_$verb");
+	if (defined($acl)) {
+		return $acl($self, $verb, @args);
 	}
 
 	return 0;
