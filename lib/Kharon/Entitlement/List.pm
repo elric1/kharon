4fa79040a8c9ffa8f92eab01a6d0e03d63158895
Roland C. Dowdeswell <elric@imrryr.org>
2012-07-11 23:19:10 +0100

    Introducing Kharon::Entitlement::List and Kharon::Entitlement::Or.

---
+++ lib/Kharon/Entitlement/List.pm
@@ -0,0 +1,69 @@
+#
+# This entitlement is used as a base class for entitlements that operate
+# on a list of entitlements.  It's check routine throws an exception.
+
+package Kharon::Entitlement::List;
+use base qw(Kharon::Entitlement);
+
+use UNIVERSAL qw(isa);
+
+use warnings;
+use strict;
+
+sub new {
+	my ($proto, %args) = @_;
+	my $class = ref($proto) || $proto;
+
+	my $self = $class->SUPER::new(%args);
+	$self->{subobjects} = [];
+
+	if (exists($args{subobjects}) && defined($args{subobjects})) {
+		if (ref($args{subobjects}) ne 'ARRAY') {
+			# XXXrcd: better error needed!
+			die "subobjects must be an ARRAY ref";
+		}
+
+		$self->set_subobjects(@{$args{handlers}});
+	}
+
+	return $self;
+}
+
+sub set_creds {
+	my ($self, @creds) = @_;
+	my $subobjs = $self->{subobjects};
+
+	for my $obj (@$subobjs) {
+		$obj->set_creds(@creds);
+	}
+	return $self->SUPER::set_creds(@creds);
+}
+
+sub set_subobjects {
+	my ($self, @subobjs) = @_;
+
+	for my $obj (@subobjs) {
+		if (!UNIVERSAL::isa($obj, 'Kharon::Entitlement')) {
+			die "Woah, bad subobject passed.  Must be a " .
+			    "Kharon::Entitlement";
+		}
+	}
+
+	$self->{subobjects} = \@subobjs;
+
+	# We don't want to catch errors...
+
+	for my $obj (@subobjs) {
+		$obj->set_opt('throw', 0);
+	}
+
+	$self->set_creds(@{$self->{credlist}});
+}
+
+sub check1 {
+	my ($self, @args) = @_;
+
+	die "Kharon::Entitlement::List does not implement check or check1";
+}
+
+1;
---
+++ lib/Kharon/Entitlement/Or.pm
@@ -0,0 +1,32 @@
+#
+# This entitlement allows one to evaluate a list of entitlements returning
+# true if any of them evaluate to true.  The list is evaluated in order but
+# the result is [obviously] independent of order.
+
+package Kharon::Entitlement::Or;
+use base qw(Kharon::Entitlement::List);
+
+use UNIVERSAL qw(isa);
+
+use warnings;
+use strict;
+
+sub check1 {
+	my ($self, @args) = @_;
+	my $subobjs = $self->{subobjects};
+	my $ret;
+	my @errs;
+
+	for my $obj (@$subobjs) {
+		$ret = $obj->check(@args);
+
+		return 1 if defined($ret) && $ret eq '1';
+
+		push(@errs, $ret) if defined($ret) && $ret ne '0';
+	}
+
+	return join(', ', @errs)	if @errs > 0;
+	return 0;
+}
+
+1;
