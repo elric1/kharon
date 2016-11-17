elric1/kharon

commit 8b571839ec8024fe44b7f39cf4500f0fa0173514

Parsing error: empty smtplike splist caused return value to be lost.

ArrayHash/arrayhash.c
@@ -617,6 +617,7 @@ state_smtplike(struct self *self, int c)
 	case '.':
 		if (code < 1000)
 			self->done = 1;
+		self->code = code;
 		/*FALLTHROUGH*/
 
 	case '-':
@@ -638,7 +639,6 @@ state_smtplike(struct self *self, int c)
 			return;
 		}
 		self->p.remnant = c;
-		self->code = code - 1000;
 		/* XXXrcd: should we check if lines have matching codes? */
 		pop(st);
 		break;

t/Protocols.t
@@ -1,6 +1,7 @@
 #
 #
 
+use Data::Dumper;
 use Test::More;
 
 use Kharon::Protocol::ArrayHash;
@@ -29,9 +30,7 @@ sub encode_parse {
 
 	ok(!$@, "encode_parse: $@");
 
-#	compare(\@vars, \@result);
-# the above line is correct: XXXrcd
-	is_deeply(\@vars, @result);
+	is_deeply([$code, @result], [250, \@vars]);
 }
 
 sub marshalling {

commit 7007a60f88fc2394d920e8507530e3a75d4ae60f

8b571839 introduced an error in the numeric return code when a line contained a leading scalar starting with a dot.

ArrayHash/arrayhash.c
@@ -395,14 +395,28 @@ static inline int
 stack_get_code(struct stack **st)
 {
 
-	return ST_ENTRY(st).u.code;
+	return ST_ENTRY(st).u.smtplike.code;
 }
 
 static inline void
 stack_set_code(struct stack **st, int code)
 {
 
-	ST_ENTRY(st).u.code = code;
+	ST_ENTRY(st).u.smtplike.code = code;
+}
+
+static inline int
+stack_get_complete(struct stack **st)
+{
+
+	return ST_ENTRY(st).u.smtplike.complete;
+}
+
+static inline void
+stack_set_complete(struct stack **st, int complete)
+{
+
+	ST_ENTRY(st).u.smtplike.complete = complete;
 }
 
 static inline ssp_val *
@@ -590,9 +604,22 @@ state_smtplike(struct self *self, int c)
 
 	code = stack_get_code(st);
 
-	D(fprintf(stderr, "state_smtplike code=%d got '%c'\n", code, c));
+	D(fprintf(stderr, "state_smtplike code=%d done=%d got '%c'\n",
+	    code, self->done, c));
 
-	if (code < 1000 && c >= '0' && c <= '9') {
+	if (c == SPACE) {
+		if (code < 100)
+			self->done = BAD;
+		return;
+	}
+
+	if (stack_get_complete(st)) {
+		self->p.remnant = c;
+		pop(st);
+		return;
+	}
+
+	if (c >= '0' && c <= '9') {
 		code *= 10;
 		code += c - '0';
 
@@ -606,42 +633,21 @@ state_smtplike(struct self *self, int c)
 	}
 
 	switch (c) {
-	case SPACE:
-		if (code < 100) {
-			self->done = BAD;
-			return;
-		}
-
-		break;
-
 	case '.':
-		if (code < 1000)
-			self->done = 1;
+		self->done = 1;
 		self->code = code;
 		/*FALLTHROUGH*/
 
 	case '-':
+		stack_set_complete(st, 1);
 		if (code < 100) {
 			self->done = BAD;
 			return;
 		}
-
-		if (code < 1000) {
-			code += 1000;
-			stack_set_code(st, code);
-			break;
-		}
-		/*FALLTHROUGH*/
-
+		return;
 	default:
-		if (code < 1100) {
-			self->done = BAD;
-			return;
-		}
-		self->p.remnant = c;
-		/* XXXrcd: should we check if lines have matching codes? */
-		pop(st);
-		break;
+		self->done = BAD;
+		return;
 	}
 }
 

ArrayHash/arrayhash.h
@@ -49,9 +49,12 @@ typedef void *ssp_val;
 struct entry {
 	union {
 		ssp_val	ret;
-		int	code;
+		struct {
+			uint32_t code;
+			uint32_t complete;
+		} smtplike;
 	} u;
-	int		state;
+	uint32_t state;
 };
 
 

t/Protocols.t
@@ -71,9 +71,11 @@ sub test_protocol {
 	encode_parse($proto, undef);
 	encode_parse($proto, '');
 	encode_parse($proto, '', '', 'a');
-	encode_parse($proto, '!');
 	encode_parse($proto, join('', map { chr($_) } (0..255)));
 	encode_parse($proto, map { chr($_) } (0..255));
+	for my $i (map { chr($_) } (0..255)) {
+		encode_parse($proto, $i);
+	}
 
 	# XXXrcd: Perl parser can't deal with these:
 	if ("$class1 $class2" !~ /Perl/) {